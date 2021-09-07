bd1a4e414d775e6a3e46aaa10187e280c1ef1569
jwhodge <jonathan@jonathanhodge.co.nz>
2021-09-07 16:38:53 +0800

    updated styling and react code prep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import './App.css';
 
+const padArr = [
+  {id: "Pad-1", triggerKey: "Q", icon: 'Kick'}, 
+  {id: "Pad-2", triggerKey: "W", icon: 'Snare'}, 
+  {id: "Pad-3", triggerKey: "E", icon: 'HiHat'}, 
+  {id: "Pad-4", triggerKey: "A", icon: 'Tom 1'}, 
+  {id: "Pad-5", triggerKey: "S", icon: 'Tom 2'}, 
+  {id: "Pad-6", triggerKey: "D", icon: 'Ride'}, 
+  {id: "Pad-7", triggerKey: "Z", icon: 'Crash'}, 
+  {id: "Pad-8", triggerKey: "X", icon: 'Floor Tom'}, 
+  {id: "Pad-9", triggerKey: "C", icon: 'Fill'} 
+];
+
 function App() {
   return (
     <div className="App">
       <div className="DmWrapper">
         <div className="Interface">
+          <Header />
+          <Pad arg={padArr[0]}/>
+          <Pad arg={padArr[1]}/>
+          <Pad arg={padArr[2]}/>
+          <Pad arg={padArr[3]}/>
+          <Pad arg={padArr[4]}/>
+          <Pad arg={padArr[5]}/>
+          <Pad arg={padArr[6]}/>
+          <Pad arg={padArr[7]}/>
+          <Pad arg={padArr[8]}/>
+          <Controls />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Header (){
+  return (
           <div className="Header">
-            <h1>OCTAPAD</h1>
+            <h1>NONAPAD</h1>
           </div>
-          <div className="Pad" id="Pad-1"></div>
-          <div className="Pad" id="Pad-2"></div>
-          <div className="Pad" id="Pad-3"></div>
-          <div className="Pad" id="Pad-4"></div>
-          <div className="Pad" id="Pad-5"></div>
-          <div className="Pad" id="Pad-6"></div>
-          <div className="Pad" id="Pad-7"></div>
-          <div className="Pad" id="Pad-8"></div>
-          <div className="Pad" id="Pad-9"></div>
+  )
+}
+
+function Pad (props) {
+  const arg = props.arg;
+  return (
+    <div className="Pad" id={arg.id}>
+      {/* <i className="padIcon">{arg.icon}</i> */}
+      <p className="padTriggerKey">{arg.triggerKey}</p>
+    </div>
+  )
+}
+
+function Controls () {
+  return (
           <div className="Controls">
             <div className="Logo">
+              <h3>Boland</h3>
               <p>PAD-9</p>
             </div>
             <div className="Display">
@@ -34,10 +72,9 @@ function App() {
               <p>POWER</p>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  );
+
+  )
 }
+
 
 export default App;
