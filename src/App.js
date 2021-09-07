3585e7883a202115dbc32f833d9a2602ea1ea8e8
jwhodge <jonathan@jonathanhodge.co.nz>
2021-09-07 14:21:51 +0800

    initial css no functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="DmWrapper">
+        <div className="Interface">
+          <div className="Header">
+            <h1>OCTAPAD</h1>
+          </div>
+          <div className="Pad" id="Pad-1"></div>
+          <div className="Pad" id="Pad-2"></div>
+          <div className="Pad" id="Pad-3"></div>
+          <div className="Pad" id="Pad-4"></div>
+          <div className="Pad" id="Pad-5"></div>
+          <div className="Pad" id="Pad-6"></div>
+          <div className="Pad" id="Pad-7"></div>
+          <div className="Pad" id="Pad-8"></div>
+          <div className="Pad" id="Pad-9"></div>
+          <div className="Controls">
+            <div className="Logo">
+              <p>PAD-9</p>
+            </div>
+            <div className="Display">
+              <p>DISPLAY</p>
+            </div>
+            <div className="Patch">
+              <p>PATCH</p>
+            </div>
+            <div className="Volume">
+              <p>VOLUME</p>
+            </div>
+            <div className="Power">
+              <p>POWER</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
